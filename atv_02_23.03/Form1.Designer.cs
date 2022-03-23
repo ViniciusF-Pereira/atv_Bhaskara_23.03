1a91659a2c6b6dfd463cd1b2a65b48139876d1df
Vinicius de Freitas Pereira <vf.pereirav1@outlook.com>
2022-03-23 12:20:04 -0300

    Atualizado 1.0 13:35Adicionar arquivos de projeto.

---
+++ atv_02_23.03/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace atv_02_23._03
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.txtValor1 = new System.Windows.Forms.TextBox();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.txtValor3 = new System.Windows.Forms.TextBox();
+            this.lblValor1 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblEquacao = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblR = new System.Windows.Forms.Label();
+            this.lblS = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl1p2 = new System.Windows.Forms.Label();
+            this.lbl1p1 = new System.Windows.Forms.Label();
+            this.lblSeta = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btCalcular.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btCalcular.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btCalcular.Location = new System.Drawing.Point(520, 28);
+            this.btCalcular.Margin = new System.Windows.Forms.Padding(4);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(191, 61);
+            this.btCalcular.TabIndex = 0;
+            this.btCalcular.Text = "CALCULAR";
+            this.btCalcular.UseVisualStyleBackColor = false;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
+            // 
+            // txtValor1
+            // 
+            this.txtValor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtValor1.Location = new System.Drawing.Point(29, 52);
+            this.txtValor1.Margin = new System.Windows.Forms.Padding(4);
+            this.txtValor1.Name = "txtValor1";
+            this.txtValor1.Size = new System.Drawing.Size(107, 26);
+            this.txtValor1.TabIndex = 1;
+            this.txtValor1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtValor2.Location = new System.Drawing.Point(190, 52);
+            this.txtValor2.Margin = new System.Windows.Forms.Padding(4);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(107, 26);
+            this.txtValor2.TabIndex = 2;
+            this.txtValor2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtValor3
+            // 
+            this.txtValor3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtValor3.Location = new System.Drawing.Point(356, 52);
+            this.txtValor3.Margin = new System.Windows.Forms.Padding(4);
+            this.txtValor3.Name = "txtValor3";
+            this.txtValor3.Size = new System.Drawing.Size(107, 26);
+            this.txtValor3.TabIndex = 3;
+            this.txtValor3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblValor1
+            // 
+            this.lblValor1.AutoSize = true;
+            this.lblValor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblValor1.Location = new System.Drawing.Point(190, 96);
+            this.lblValor1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblValor1.Name = "lblValor1";
+            this.lblValor1.Size = new System.Drawing.Size(0, 20);
+            this.lblValor1.TabIndex = 4;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(41, 321);
+            this.lblResultado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 20);
+            this.lblResultado.TabIndex = 5;
+            // 
+            // lblEquacao
+            // 
+            this.lblEquacao.AutoSize = true;
+            this.lblEquacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.lblEquacao.Location = new System.Drawing.Point(41, 164);
+            this.lblEquacao.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEquacao.Name = "lblEquacao";
+            this.lblEquacao.Size = new System.Drawing.Size(0, 20);
+            this.lblEquacao.TabIndex = 6;
+            this.lblEquacao.Click += new System.EventHandler(this.lblEquacao_Click);
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblB.Location = new System.Drawing.Point(41, 197);
+            this.lblB.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(0, 20);
+            this.lblB.TabIndex = 7;
+            // 
+            // lblR
+            // 
+            this.lblR.AutoSize = true;
+            this.lblR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblR.Location = new System.Drawing.Point(29, 96);
+            this.lblR.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblR.Name = "lblR";
+            this.lblR.Size = new System.Drawing.Size(119, 20);
+            this.lblR.TabIndex = 8;
+            this.lblR.Text = "RESOLUÇÃO";
+            this.lblR.Visible = false;
+            // 
+            // lblS
+            // 
+            this.lblS.AutoSize = true;
+            this.lblS.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblS.Location = new System.Drawing.Point(36, 270);
+            this.lblS.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblS.Name = "lblS";
+            this.lblS.Size = new System.Drawing.Size(108, 24);
+            this.lblS.TabIndex = 9;
+            this.lblS.Text = "SOLUÇÃO";
+            this.lblS.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(29, 28);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 21);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Valor de A";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(190, 28);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 21);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Valor de B";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(356, 28);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 21);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Valor de C";
+            // 
+            // lbl1p2
+            // 
+            this.lbl1p2.AutoSize = true;
+            this.lbl1p2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl1p2.Location = new System.Drawing.Point(496, 197);
+            this.lbl1p2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1p2.Name = "lbl1p2";
+            this.lbl1p2.Size = new System.Drawing.Size(0, 20);
+            this.lbl1p2.TabIndex = 14;
+            // 
+            // lbl1p1
+            // 
+            this.lbl1p1.AutoSize = true;
+            this.lbl1p1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.lbl1p1.Location = new System.Drawing.Point(496, 164);
+            this.lbl1p1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl1p1.Name = "lbl1p1";
+            this.lbl1p1.Size = new System.Drawing.Size(0, 20);
+            this.lbl1p1.TabIndex = 13;
+            this.lbl1p1.Click += new System.EventHandler(this.lbl1p1_Click);
+            // 
+            // lblSeta
+            // 
+            this.lblSeta.AutoSize = true;
+            this.lblSeta.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblSeta.Location = new System.Drawing.Point(373, 167);
+            this.lblSeta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSeta.Name = "lblSeta";
+            this.lblSeta.Size = new System.Drawing.Size(56, 47);
+            this.lblSeta.TabIndex = 16;
+            this.lblSeta.Text = "➜";
+            this.lblSeta.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lblSeta.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(520, 298);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 61);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "LIMPAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 368);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblSeta);
+            this.Controls.Add(this.lbl1p2);
+            this.Controls.Add(this.lbl1p1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblS);
+            this.Controls.Add(this.lblR);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblEquacao);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblValor1);
+            this.Controls.Add(this.txtValor3);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.txtValor1);
+            this.Controls.Add(this.btCalcular);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Calculadora Bhaskara";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btCalcular;
+        private TextBox txtValor1;
+        private TextBox txtValor2;
+        private TextBox txtValor3;
+        private Label lblValor1;
+        private Label lblResultado;
+        private Label lblEquacao;
+        private Label lblB;
+        private Label lblR;
+        private Label lblS;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label lbl1p2;
+        private Label lbl1p1;
+        private Label lblSeta;
+        private Button button1;
+    }
+}
---
+++ atv_02_23.03/Form1.cs
@@ -0,0 +1,126 @@
+﻿namespace atv_02_23._03
+{
+
+    /* Atividade feita dia 23/03/2022 
+    *Aluno Vinicius de Freitas Pereira
+    *RA: 2222106123
+
+
+    */
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btCalcular_Click(object sender, EventArgs e)
+        {
+            lblR.Visible = true;
+            lblS.Visible = true;
+            lblSeta.Visible = true;
+            button1.Visible = true;
+
+
+            int.TryParse(txtValor1.Text, out var valorA0);
+            int.TryParse(txtValor2.Text, out var valorB0);
+            int.TryParse(txtValor3.Text, out var valorC0);
+            int.TryParse(txtValor1.Text, out var valorA);
+            int.TryParse(txtValor2.Text, out var valorB);
+            int.TryParse(txtValor3.Text, out var valorC);
+
+            int delta = (valorB * valorB) - (4 * valorA * valorC);
+
+          
+            int.TryParse(txtValor3.Text, out var ValorCpositivo);
+
+                if (ValorCpositivo > 0)
+                {
+                    lblValor1.Text = "DE  (" + txtValor1.Text + "x²) + (" + txtValor2.Text + "x)" + " + " + txtValor3.Text + " = 0";
+                }
+                else
+                {
+                    lblValor1.Text = "DE  (" + txtValor1.Text + "x²) + (" + txtValor2.Text + "x) " + txtValor3.Text + " = 0";
+
+                }
+
+
+
+                int.TryParse(txtValor1.Text, out var valor1);
+
+            if (valor1 != 0)
+            {
+
+
+                if (delta < 0)
+                {
+
+                    MessageBox.Show("O valor de delta é " + delta + ", ou seja negativo, portando as raizes não podem ser calcualdas !", "Erro!");
+                    lblResultado.Text = " SEM SOLUÇÃO";
+
+                }
+                else
+                {
+
+                    float Resultado = (((valorB * (-1))) + Convert.ToSingle(Math.Sqrt(delta))) / (2 * valorA);
+                    float Resultado2 = (((valorB * (-1))) - Convert.ToSingle(Math.Sqrt(delta))) / (2 * valorA);
+
+
+
+                    lblResultado.Text = "X¹ = " + Resultado.ToString() + " e X² = " + Resultado2.ToString();
+                }
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Preencha o campo A com um valor diferente de 0!", "Erro!");
+
+                lblResultado.Text = " SEM SOLUÇÃO, \n RAIZ QUADRADA IMPOSSIVEL DE CALCULAR";
+                }
+
+                lblEquacao.Text = "X =  - (" + valorB + ") ± √ (" + valorB + "²) - " + "4 * (" + valorA + ") * (" + valorC + ")";
+                lblB.Text = "                 2 * (" + valorA + ")";
+
+                lbl1p1.Text = "X =  - (" + valorB + ") ± √ (" + delta + ")";
+                lbl1p2.Text = "               "+  2 * valorA ;
+
+
+
+        }
+
+        private void lbl1p1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblSeta.Visible = false;
+            lblS.Visible = false;
+            lblR.Visible = false;
+            button1.Visible = false;
+            txtValor1.Text = "";
+            txtValor2.Text = "";
+            txtValor3.Text = "";
+            lblEquacao.Text = null;
+            lblResultado.Text = null;
+            lbl1p1.Text = null;
+            lbl1p2.Text = null;
+            lblValor1.Text=null;
+            lblB.Text=null;
+
+
+        }
+
+       }
+    
+    
+}
